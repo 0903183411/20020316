de828b1f2941ffda92b874b0d3a72cc866796e8e
YiChin <0123456ghjkl@gmail.com>
2022-03-16 16:14:09 +0800

    加入專案檔案。

---
+++ App0309/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App0309
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            Console.WriteLine("請輸入一個分數");
+            score = Convert.ToInt32(Console.ReadLine());
+            string result = score >= 60 ? "及格" : "不及格";
+            Console.WriteLine("同學你{0}",result);
+            Console.ReadLine();
+        }
+    }
+}
